652345a6005b3bd858947c06e09f5f97e9466b07
unknown <arouisaac1@gmail.com>
2024-02-05 19:36:38 +0300

    Implementation of BonusCalculator Class

--- Arou ISAAC/Refactor.py
+++ Arou ISAAC/Refactor.py
@@ -35,6 +35,10 @@ if __name__ == "__main__":
     report_generator.generate_report(manager)
     report_generator.generate_report(developer)
         
+    bonus_calculator = BonusCalculator()
+    manager_bonus = bonus_calculator.calculate_bonus(manager)
+    developer_bonus = bonus_calculator.calculate_bonus(developer)
+
 
 
 

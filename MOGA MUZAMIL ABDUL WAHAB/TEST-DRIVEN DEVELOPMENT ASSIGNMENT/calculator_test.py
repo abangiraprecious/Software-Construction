98e50161f78a2e6800795a37471f7661b592ebc4
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-05 11:53:49 +0300

    add test for calculator 2

--- MOGA MUZAMIL ABDUL WAHAB/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_test.py
+++ MOGA MUZAMIL ABDUL WAHAB/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_test.py
@@ -15,3 +15,18 @@ class TestCalculator(unittest.TestCase):
         calc = Calculator()
         self.assertEqual(calc.subtract(5, 3), 2)
 
+    def test_multiplication(self): #Multiplying two numbers.
+        calc = Calculator()
+        self.assertEqual(calc.multiply(2, 3), 6)
+
+    def test_division(self):
+        calc = Calculator()
+        self.assertEqual(calc.divide(6, 3), 2)
+
+    def test_division_by_zero(self): #Raising ValueError if number2 is zero.
+        calc = Calculator()
+        with self.assertRaises(ValueError):
+            calc.divide(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()

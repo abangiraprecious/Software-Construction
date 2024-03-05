4598e4b9fbb60c6d7de050d5e23c7d6f50af2711
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-05 11:51:53 +0300

    refactor calculator 2

--- MOGA MUZAMIL ABDUL WAHAB/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_refactored.py
+++ MOGA MUZAMIL ABDUL WAHAB/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_refactored.py
@@ -8,4 +8,12 @@ class Calculator_Refactored:
     def subtract(number1, number2):
         return number1 - number2
 
- 
+    @staticmethod
+    def multiply(number1, number2):
+        return number1 * number2
+
+    @staticmethod
+    def divide(number1, number2):
+        if number2 == 0:
+            raise ValueError("Cannot divide by zero.")
+        return number1 / number2

39e6d9c7a8f334b0e2ec50f3ee26d13149a07b4b
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-05 11:51:04 +0300

    refactor calculator

---
+++ MOGA MUZAMIL ABDUL WAHAB/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_refactored.py
@@ -0,0 +1,11 @@
+class Calculator_Refactored:
+    """The calculator refactored class contains methods for basic arithmetic operations."""
+    @staticmethod
+    def add(number1, number2 ):
+        return number1 + number2 
+
+    @staticmethod
+    def subtract(number1, number2):
+        return number1 - number2
+
+ 

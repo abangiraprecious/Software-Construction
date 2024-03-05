f0560dfa0bfd1f18fe401aef70b4733e62328bd1
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-05 11:57:54 +0300

    add calculator division functionality

--- MOGA MUZAMIL ABDUL WAHAB/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator.py
+++ MOGA MUZAMIL ABDUL WAHAB/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator.py
@@ -9,4 +9,7 @@ class Calculator:
     def multiply(self, number1 , number2 ):
         return number1  * number2 
 
-  
+    def divide(self, number1 , number2 ):
+        if number2  == 0:
+            raise ValueError("Cannot divide by zero.")
+        return number1 / number2 

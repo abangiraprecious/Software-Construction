b2b9e4ce3d26aeea544f2fc40ae8ba075ac7b63a
NajjobaTracy <tracynajjoba8@gmail.com>
2024-03-05 11:38:35 +0300

    calculator test code

---
+++ TRACY NAJJOBA/assignment three/calculator_test.py
@@ -0,0 +1,33 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+
+    def __init__(self):
+        """Initializes the calculator object."""
+        pass
+
+    def test_addition(self):#Adds two numbers.
+        calc = Calculator()
+        self.assertEqual(calc.add(2, 3), 5)
+
+    def test_subtraction(self): #Subtracts two numbers.
+        calc = Calculator()
+        self.assertEqual(calc.subtract(5, 3), 2)
+
+    def test_multiplication(self): #Multiplies two numbers.
+        calc = Calculator()
+        self.assertEqual(calc.multiply(2, 3), 6)
+
+    def test_division(self):
+        calc = Calculator()
+        self.assertEqual(calc.divide(6, 3), 2)
+
+    def test_division_by_zero(self): #Raises ValueError if num2 is zero.
+        calc = Calculator()
+        with self.assertRaises(ValueError):
+            calc.divide(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()
+    

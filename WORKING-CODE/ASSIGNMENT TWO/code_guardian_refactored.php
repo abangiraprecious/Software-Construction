51618581c2f9f6e6474cb640d084a10edbd3441e
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-12 01:16:50 +0300

    feat: add functionality to validate the password

--- WORKING-CODE/ASSIGNMENT TWO/code_guardian_refactored.php
+++ WORKING-CODE/ASSIGNMENT TWO/code_guardian_refactored.php
@@ -57,7 +57,10 @@ class Authentication {
         }
     }
 
-  
+    private function isValidPassword($password) {
+        // Strong password validation for entered password
+        return preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/', $password);
+    }
 
 }
 ?>

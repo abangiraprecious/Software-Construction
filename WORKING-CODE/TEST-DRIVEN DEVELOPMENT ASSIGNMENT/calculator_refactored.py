80cfbd13fa47b30667c2b9a1417025b131f1c644
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-12 01:41:28 +0300

    feat: add functionality to perform subtraction  of numbers

--- WORKING-CODE/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_refactored.py
+++ WORKING-CODE/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_refactored.py
@@ -3,3 +3,7 @@ class Calculator_Refactored:
     @staticmethod
     def add(number1, number2 ):
         return number1 + number2 
+
+    @staticmethod
+    def subtract(number1, number2):
+        return number1 - number2

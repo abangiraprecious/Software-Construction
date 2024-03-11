7c66d78572af7c5445b638c3a4938f2e194f5020
AROU ISAAC <arouisaac1@gmail.com>
2024-03-11 21:18:01 +0300

    Implement constructor to initialize database connection within the Authentication class

--- Arou ISAAC/Assignment_two/code_guardians_refactored.php
+++ Arou ISAAC/Assignment_two/code_guardians_refactored.php
@@ -3,3 +3,7 @@
 class Authentication {
     // connection to the database connection 
     private $db; 
+
+    public function __construct($db) {
+        $this->db = $db; // Storing  the database connection
+    }

43f9abfc8948cda57477800536bc28006b5019d5
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-12 01:11:38 +0300

    Add and initialize authentication class

---
+++ WORKING-CODE/ASSIGNMENT TWO/code_guardian_refactored.php
@@ -0,0 +1,13 @@
+<?php
+
+class Authentication {
+    // connection to the database connection 
+    private $db; 
+
+    public function __construct($db) {
+        $this->db = $db; // Storing  the database connection
+    }
+
+
+}
+?>

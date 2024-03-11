f8ba58908aeeae61cdff11140080934deb9b8557
MogaMAW <moga.m.abdulwahab@gmail.com>
2024-03-12 01:41:07 +0300

    feat: add functionality to perform addition of numbers

--- WORKING-CODE/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_refactored.py
+++ WORKING-CODE/TEST-DRIVEN DEVELOPMENT ASSIGNMENT/calculator_refactored.py
@@ -1,2 +1,5 @@
 class Calculator_Refactored:
     """The calculator refactored class contains methods for basic arithmetic operations."""
+    @staticmethod
+    def add(number1, number2 ):
+        return number1 + number2 
